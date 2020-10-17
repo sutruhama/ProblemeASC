3e73d6314f568eef4288585d1248abc05641e0d7
sutruhama <72358545+sutruhama@users.noreply.github.com>
2020-10-17 13:20:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moorelaw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double x;
+            n= int.Parse(Console.ReadLine());
+            x = Math.Log(n, 2) * 1.5;
+            Console.WriteLine(x + " " + "ani");
+            Console.ReadKey();
+            
+            
+        }
+    }
+}
