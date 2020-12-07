ed8561ad8afee1065e09eba66cd1bab52114fdf2
sutruhama <72358545+sutruhama@users.noreply.github.com>
2020-12-07 17:01:54 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace moorelaw
+namespace p1._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n;
-            double x;
-            n= int.Parse(Console.ReadLine());
-            x = Math.Log(n, 2) * 1.5;
-            Console.WriteLine(x + " " + "ani");
+            int a, b, x;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            if (a == 0)
+                if (b == 0)
+                    Console.WriteLine("Exista o infinitate de solutii");
+                else
+                    Console.WriteLine("Ecuatie imposibila");
+            else
+            {
+                x = -b / a;
+                Console.WriteLine("Solutia ecuatiei este " + " " + x);
+            }
             Console.ReadKey();
-            
-            
+
+
         }
     }
 }
