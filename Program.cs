7c67a499889903de8b23eeb0f097c44a558e3265
sutruhama <72358545+sutruhama@users.noreply.github.com>
2020-12-07 17:16:00 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace p1._1
+namespace moorelaw
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int a, b, x;
-            a = int.Parse(Console.ReadLine());
-            b = int.Parse(Console.ReadLine());
-            if (a == 0)
-                if (b == 0)
-                    Console.WriteLine("Exista o infinitate de solutii");
-                else
-                    Console.WriteLine("Ecuatie imposibila");
-            else
-            {
-                x = -b / a;
-                Console.WriteLine("Solutia ecuatiei este " + " " + x);
-            }
+            int n;
+            double x;
+            n= int.Parse(Console.ReadLine());
+            x = Math.Log(n, 2) * 1.5;
+            Console.WriteLine(x + " " + "ani");
             Console.ReadKey();
-
-
+            
+            
         }
     }
 }
